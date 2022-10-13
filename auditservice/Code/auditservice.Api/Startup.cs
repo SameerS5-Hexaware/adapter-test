708eb038bbd916e656c222c05e321f3ab026cd0a
SameerS5-Hexaware <70312266+SameerS5-Hexaware@users.noreply.github.com>
2022-10-13 15:48:44 +0530

    Update Startup.cs

--- auditservice/Code/auditservice.Api/Startup.cs
+++ auditservice/Code/auditservice.Api/Startup.cs
@@ -3,7 +3,7 @@ using auditservice.Api.Middleware;
 using ElmahCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration; 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
